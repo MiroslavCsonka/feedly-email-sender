766b47930d219ab87eebb46da7d45f6921717bc3
Miroslav Csonka <miroslav.csonka@gmail.com>
2020-05-17 16:40:56 +0100

    Refactor duplication

--- src/lambda_function.rb
+++ src/lambda_function.rb
@@ -9,27 +9,23 @@ FROM_EMAIL = 'miroslavcsonka@miroslavcsonka.com'
 TO_EMAIl = 'miroslavcsonka@miroslavcsonka.com'
 SERVICE_TO_RUN = ENV.fetch('SERVICE')
 
-def run_feedly
-  feedly_client = Feedly.new(
-    ENV.fetch('FEEDLY_AUTH_TOKEN'),
-    ENV.fetch('SAVED_LATER_STREAM_ID')
-  )
-  response = feedly_client.items
+def run(client, link_field_name)
+  items_response = client.items
 
   output = {
-    get_stream: { code: response.code, body: JSON.parse(response.body) }
+    get_stream: { code: items_response.code, body: JSON.parse(items_response.body) }
   }
 
-  unless response.code == '200'
-    return format_response(response.code.to_i, "Getting items failed with #{response.code} and body #{response.body}")
+  unless items_response.code == '200'
+    return format_response(items_response.code.to_i, "Getting items failed with #{items_response.code} and body #{items_response.body}")
   end
 
-  item = JSON.parse(response.body).fetch('items')[0]
+  item = JSON.parse(items_response.body).fetch('items')[0]
 
   return format_response(400, 'No more items to serve') unless item
 
   output[:item] = item
-  title, url = item.values_at('title', 'canonicalUrl')
+  title, url = item.values_at('title', link_field_name)
 
   email_sent = send_email(
     from: FROM_EMAIL,
@@ -42,45 +38,8 @@ def run_feedly
   return format_response(400, output) unless email_sent
 
   if email_sent
-    delete_tag_response = feedly_client.delete(item)
-    output[:delete_tag_response] = { code: delete_tag_response.code, body: delete_tag_response.body }
-  end
-
-  format_response(200, { response: output })
-end
-
-def run_raindrop
-  raindrop_client = Raindrop.new(ENV.fetch('RAINDROP_AUTH_TOKEN'))
-  response = raindrop_client.items
-
-  output = {
-    get_stream: { code: response.code, body: JSON.parse(response.body) }
-  }
-
-  unless response.code == '200'
-    return format_response(response.code.to_i, "Getting items failed with #{response.code} and body #{response.body}")
-  end
-
-  item = JSON.parse(response.body).fetch('items')[0]
-
-  return format_response(400, 'No more items to serve') unless item
-
-  output[:item] = item
-  title, url = item.values_at('title', 'link')
-
-  email_sent = send_email(
-    from: FROM_EMAIL,
-    to: TO_EMAIl,
-    subject: "Read: #{title}",
-    body: url
-  )
-  output[:email_sending] = email_sent
-
-  return format_response(400, output) unless email_sent
-
-  if email_sent
-    delete_tag_response = raindrop_client.delete(item)
-    output[:delete_tag_response] = { code: delete_tag_response.code, body: delete_tag_response.body }
+    delete_item_response = client.delete(item)
+    output[:delete_tag_response] = { code: delete_item_response.code, body: delete_item_response.body }
   end
 
   format_response(200, { response: output })
@@ -88,9 +47,13 @@ end
 
 def lambda_handler(event:, context:)
   if SERVICE_TO_RUN == 'feedly'
-    run_feedly
+    feedly_client = Feedly.new(
+      ENV.fetch('FEEDLY_AUTH_TOKEN'),
+      ENV.fetch('SAVED_LATER_STREAM_ID')
+    )
+    run(feedly_client, 'canonicalUrl')
   elsif SERVICE_TO_RUN == 'raindrop'
-    run_raindrop
+    run(Raindrop.new(ENV.fetch('RAINDROP_AUTH_TOKEN')), 'link')
   else
     format_response(400, { error: "Unknown service #{SERVICE_TO_RUN}" })
   end
